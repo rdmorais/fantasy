406cbd5abc7ea7643c8ffd7f4e5919c1c7c6023c
Rodrigo Morais <rduartemorais@gmail.com>
2021-05-18 20:03:49 -0300

    Inicio do projeto

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView style={styles.background}>
+      <View style={styles.container}>
+        <TextInput style={styles.input} placeholder="Email"/>
+
+        <TextInput style={styles.input} placeholder="Senha"/>
+
+        <TouchableOpacity>
+          <Text>Entrar</Text>
+        </TouchableOpacity>
+
+        
+        <TouchableOpacity>
+          <Text>Cadastrar</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '90%'
   },
+  background: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#5a8ed1'
+  },
+  input: {
+    backgroundColor: '#ffffff',
+    width: '90%',
+    marginBottom: 15,
+    fontSize: 17,
+    borderRadius: 7,
+    borderColor: 'black',
+    borderWidth: 1.5
+  } 
 });
